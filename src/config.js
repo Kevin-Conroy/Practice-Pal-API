5a036aced072965099351a093a6cc71409d07ad9
Kevin Conroy <Kcdrumming@gmail.com>
2021-08-04 11:57:36 -0700

    8-4 Heroku troubleshoot

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: "postgresql://practice_pal_admin@localhost/practice_pal_api",
+    DATABASE_URL: "postgres://mugtijkvnahlfq:61a323dbf85fc8a2fea8bca008ee286d2fe738df5d4f35f35d5a41e48055b8b0@ec2-35-174-56-18.compute-1.amazonaws.com:5432/d2haivfqvhl4s4",
     JWTSECRET: process.env.JWTSECRET || 'ThisismyJWTsecret'
   }
